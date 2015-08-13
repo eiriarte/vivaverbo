5858a96a2bf453a16361443b920803186d7be69b
Eduardo I. Iriarte Gahete <eiriarte@gmail.com>
2015-08-13 16:52:58 +0200

    Elimina literales obsoletos/de prueba
    Elimina literales obsoletos/de prueba y sus traducciones:
    
    "Hola, <usuario>"
    "Main DIV"
    …

--- client/app/translations.js
+++ client/app/translations.js
@@ -1,5 +1,5 @@
 angular.module('vivaverboApp').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('eo', {"Hello {{ getCurrentUser().name }}":"Saluton {{ getCurrentUser().name }}","Logout":"Elsaluti","Main DIV":"Ĉefa DIV"});
+    gettextCatalog.setStrings('eo', {"Logout":"Elsaluti"});
 /* jshint +W100 */
 }]);
