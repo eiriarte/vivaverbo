cbe18e891713fa2277128f330ee8b1e42b00a09d
Eduardo I. Iriarte Gahete <eiriarte@gmail.com>
2015-10-09 17:25:31 +0200

    Incorpora Navegador y SO a los logs
    Mejora el formato de los logs sustituyendo la cadena user-agent por sólo
    el Navegador y el Sistema Operativo en formato:
    
    - `"browser/version (OS/version)"`
    
    Además añade unidades al tiempo de ejecución y el tamaño de la respuesta
    (ms y B, respectivamente).

--- server/config/logging.js
+++ server/config/logging.js
@@ -4,25 +4,37 @@
 
 'use strict';
 
+var parser = require('ua-parser-js');
 var morgan = require('morgan');
 var _ = require('lodash');
 
-var logFormat = ':remote-addr ":vv-user" [:date[iso]] ":method :url" :status :response-time :res[content-length] ":referrer" ":user-agent"';
+var logFormat = ':remote-addr ":vv-user" [:date[iso]] ":method :url" :status :response-time ms :res[content-length] B ":referrer" ":vv-ua"';
 
 module.exports = function(app, env) {
-
   if ('production' === env) {
-    morgan.token('vv-user', function(req, res) {
-      var user = '<anon>';
-      if (req.user && _.isString(req.user.email)) {
-        user = req.user.email.split('@')[0] || '<sin email>';
-      }
-      return user;
-    });
-
+    morgan.token('vv-user', getUser);
+    morgan.token('vv-ua', getUserAgent);
     app.use(morgan(logFormat));
   } else {
     app.use(morgan('dev'));
   }
+}
 
+// Devuelve el usuario logado ('user' en 'user@example.com')
+function getUser(req, res) {
+  var user = '<anon>';
+  if (req.user && _.isString(req.user.email)) {
+    user = req.user.email.split('@')[0] || '<sin email>';
+  }
+  return user;
+}
+
+// Devuelve el navegador y sistema operativo del usuario
+function getUserAgent(req, res) {
+  var ua = parser(req.get('user-agent'));
+  var browserName = ua.browser.name || '-';
+  var browserVersion = ua.browser.major ? '/' + ua.browser.major : '';
+  var osName = ua.os.name || '-';
+  var osVersion = ua.os.version ? '/' + ua.os.version : '';
+  return browserName + browserVersion + '(' + osName + osVersion + ')';
 }
