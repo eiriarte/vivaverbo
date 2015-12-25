844ad3665749119e409e3b0ec0115ec29b62b097
Eduardo I. Iriarte Gahete <eiriarte@gmail.com>
2015-12-25 17:14:47 +0100

    Corrige la ruta de cards.js en assigntrads.js

--- data/scripts/assigntrads.js
+++ data/scripts/assigntrads.js
@@ -1,4 +1,10 @@
-cards = require('./data/fcards');
+/*
+  Asigna las traducciones de trads.js a las tarjetas de cards.js y vuelca el
+  resultado a stdout.
+
+  Uso: node assigntrads.js
+*/
+cards = require('./data/cards');
 trads = require('./data/trads');
 
 cards.forEach(function(card) {
