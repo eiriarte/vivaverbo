aad822d4dd80decfa0ab4bc37a7dbb35f3913c2d
Eduardo I. Iriarte Gahete <eiriarte@gmail.com>
2015-12-30 17:09:19 +0100

    Volver a generar missing.txt
    …con reglas más estrictas y habiendo incorporado Reta Vortaro

--- data/scripts/missing.js
+++ data/scripts/missing.js
@@ -4,29 +4,45 @@
 
   Uso: node missing.js
 */
+var _ = require('lodash');
 var cards = require('./data/cards');
 var freqs = require('./data/fq');
 
-var hash = getHash(cards);
+console.warn('Buscando palabras no registradas…');
 
-freqs.forEach(function(group, i) {
-  group.forEach(function(word) {
-    if (typeof hash[word] === 'undefined') {
-      console.log(i, word);
-    }
-  });
+var deriv = /(as|is|os|us|u|j|n)$/;
+var partic = /[aio]n?t[oea]$/;
+var ho = /(hh|sh|gh|ch|jh)/;
+var ekster = /[^a-zĥŝĝĉĵŭ,.\s\-]/;
+var hash = _.groupBy(cards, function(card) {
+  return card.respuesta.toLowerCase();
+});
+var total = 0;
+
+_.forOwn(freqs, function(freq, word) {
+  if (!_.isNumber(freq)) {
+    console.error('%s no tiene una frecuencia. Abortando…', word);
+    return false;
+  }
+  if (isValid(word) && typeof hash[word] === 'undefined' && !adj(word)) {
+    console.log('%d %s', freq, word);
+    total++;
+  }
 });
 
-function getHash(cards) {
-  var hash = {};
+console.warn('Total: %d palabras.', total);
 
-  cards.forEach(function(card) {
-    if (typeof hash[card.respuesta] !== 'undefined') {
-      console.error('"%s" está duplicada: %s', card.respuesta);
-      process.exit(1);
-    }
-    hash[card.respuesta] = card;
-  });
+function isValid(word) {
+  if (deriv.test(word)) return false;
+  if (partic.test(word)) return false;
+  if (ho.test(word)) return false;
+  if (ekster.test(word)) return false;
+  return true;
+}
 
-  return hash;
+function adj(word) {
+  if (word.slice(-1) === 'a') {
+    return (typeof hash[word.slice(0, -1) + 'o'] !== 'undefined');
+  }
+  return false;
 }
