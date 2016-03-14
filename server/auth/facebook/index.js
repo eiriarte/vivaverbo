7729b3a60d45a792a0514fb02b1c1e5fd23cd890
Eduardo I. Iriarte Gahete <eiriarte@gmail.com>
2016-03-14 10:18:29 +0100

    Configura la redirecciones del login vía Facebook
    Redirecciona a la landing page "/" en caso de fallo de autenticación.

--- server/auth/facebook/index.js
+++ server/auth/facebook/index.js
@@ -9,12 +9,12 @@ var router = express.Router();
 router
   .get('/', passport.authenticate('facebook', {
     scope: ['email', 'user_about_me'],
-    failureRedirect: '/signup',
+    failureRedirect: '/',
     session: false
   }))
 
   .get('/callback', passport.authenticate('facebook', {
-    failureRedirect: '/signup',
+    failureRedirect: '/',
     session: false
   }), auth.setTokenCookie);
 
